99d2bff0f49988a4684a568213bbe2028cebfba1
Firdaus Razak <razakfirdaus95@gmail.com>
2022-07-14 08:25:59 +0800

    first upl

---
+++ ssfworkshop13/src/main/java/com/visademo/ssfworkshop13/Ssfworkshop13Application.java
@@ -0,0 +1,13 @@
+package com.visademo.ssfworkshop13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssfworkshop13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssfworkshop13Application.class, args);
+	}
+
+}
---
+++ ssfworkshop13/src/test/java/com/visademo/ssfworkshop13/Ssfworkshop13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visademo.ssfworkshop13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssfworkshop13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
